9a67305a23d8e04384f06db5304e6015c0aa94ad
Paul Nathaniel Larkin <palark93@gmail.com>
2015-06-08 15:18:06 -0600

    Initial upload
    Initial upload of Unity project

---
+++ Assets/Calc1.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+
+public class Calc1 : MonoBehaviour {
+
+    public Text electricBill;
+    public Text numYears;
+    public Text result;
+    public Slider yearSlider;
+    public int totalSpent;
+    public int billInt;
+    public int yearsInt;
+
+    IEnumerator Calculate()
+    {
+        yield return new WaitForEndOfFrame();
+        int.TryParse(electricBill.text, out billInt);
+        yearsInt = (int)yearSlider.value;
+        numYears.text = ("" + yearsInt);
+        totalSpent = billInt * 12 * yearsInt;
+        print("$" + totalSpent + " spent over " + yearsInt + " year(s).");
+        result.text = ("$" + totalSpent + " spent over " + yearsInt + " year(s).");
+    }
+
+    void OnEnable ()
+    {
+        StartCoroutine(Calculate());
+    }
+}
