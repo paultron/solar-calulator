0b9038f5edc8a500c6ee8922a1f8e5ef6786ec5d
Paul Nathaniel Larkin <palark93@gmail.com>
2015-06-13 20:56:12 -0600

    UI overhaul
    completely redid UI, added elements, more coding

--- Assets/Calc1.cs
+++ Assets/Calc1.cs
@@ -5,27 +5,88 @@ using System.Collections;
 
 public class Calc1 : MonoBehaviour {
 
-    public Text electricBill;
-    public Text numYears;
+    int billInt;
+    int kwhInt;
+    double kwhDoub;
+    double kwhDoubDaily;
+    double kwhDoubPeak;
+    double NumPanels;
+    double PanelCost;
+    public bool isPaperBill;
+    int totalSpent;
+    public Text InputText;
+    public Text kwhNeed;
     public Text result;
-    public Slider yearSlider;
-    public int totalSpent;
-    public int billInt;
-    public int yearsInt;
+    public Text estimatedCost;
 
     IEnumerator Calculate()
     {
-        int.TryParse(electricBill.text, out billInt);
-        yearsInt = (int)yearSlider.value;
-        numYears.text = yearsInt.ToString();
-        totalSpent = billInt * 12 * yearsInt;
-        print("$" + totalSpent + " spent over " + yearsInt + " year(s).");
-        result.text = ("$" + totalSpent + " spent over " + yearsInt + " year(s).");
-        yield return new WaitForEndOfFrame();
+        //calculate total spent, decide if paper or kwh
+        if (isPaperBill)
+        {
+            //get input value monthly bill
+            int.TryParse(InputText.text, out billInt);
+            //print result of 15 and 30 year sums
+            result.text = ("$" + billInt * 12 * 15 + " spent over 15 year(s). $" + billInt * 12 * 30 + " spent over 30 year(s).");
+            //estimate monthly kwh usage
+            kwhDoub = (billInt/.12125);
+            //estimate daily kwh usage
+            kwhDoubDaily = kwhDoub/30;
+            //estimate required kwh during peak sun hours
+            kwhDoubPeak = kwhDoubDaily / 5.5;
+            kwhNeed.text = (((int)kwhDoubPeak).ToString() + "kWh needed during peak sun hours.");
+            //number of panels at 260W
+            NumPanels = kwhDoubPeak / .260;
+            //panel cost * 1.25
+            PanelCost = (NumPanels * 250) * 1.25;
+            estimatedCost.text = ("$" + ((int)PanelCost).ToString() + " estimated cost.");
+            yield return new WaitForEndOfFrame();
+        }
+        if (!isPaperBill)
+        {
+            //get input value monthly kWh 
+            double tempDoub;
+            int.TryParse(InputText.text, out kwhInt);
+            //estimate daily kwh usage
+            kwhDoubDaily = kwhInt / 30;
+            //estimate required kwh during peak sun hours
+            kwhDoubPeak = kwhDoubDaily / 5.5;
+            kwhNeed.text = (((int)kwhDoubPeak).ToString() + "kWh needed during peak sun hours.");
+            //number of panels at 260W
+            NumPanels = kwhDoubPeak / .260;
+            //panel cost * 1.25
+            PanelCost = (NumPanels * 250) * 1.25;
+            estimatedCost.text = ("$" + ((int)PanelCost).ToString() + " estimated cost.");
+            if(kwhInt > 600)
+            {
+                tempDoub = (kwhInt - 600)*.1399;
+                kwhInt = (int)tempDoub+70;
+            }
+            else
+            {
+                tempDoub = kwhInt * .11;
+                kwhInt = (int)tempDoub;
+            }
+            //print result of 15 and 30 year sums
+            result.text = ("$" + kwhInt * 12 * 15 + " spent over 15 year(s). $" + kwhInt * 12 * 30 + " spent over 30 year(s).");
+            yield return new WaitForEndOfFrame();
+        }
+
+
+
+
     }
 
-    void OnEnable ()
+    public void OnEnable ()
     {
         StartCoroutine(Calculate());
+    }
+    public void SetPaperTrue()
+    {
+        isPaperBill = true;
+    }
+    public void SetPaperFalse()
+    {
+        isPaperBill = false;
     }
 }
